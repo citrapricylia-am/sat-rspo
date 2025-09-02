0360535214e17752316cf4c5d580852d24fcd19a
citrapricylia-am <citrpam@gmail.com>
2025-09-02 17:55:23 +0700

    fixing2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
 // src/lib/api.ts
 import { createClient, type User as SupabaseAuthUser } from "@supabase/supabase-js";
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0, 5) + "...");
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL as string,
